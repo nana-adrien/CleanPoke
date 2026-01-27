b9751c03608fd957d4084d8b92d08a5b9b0be0f6
adrien nana <kakonana7@gmail.com>
2026-01-27 22:35:33 +0100

    Initial Commit

---
+++ CleanPokeTests/CleanPokeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanPokeTests.swift
+//  CleanPokeTests
+//
+//  Created by Digiprem on 27/01/2026.
+//
+
+import Testing
+@testable import CleanPoke
+
+struct CleanPokeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
