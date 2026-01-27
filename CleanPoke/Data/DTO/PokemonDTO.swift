694c84332c8ee0ff03595eac2b551ebbbe48a883
adrien nana <kakonana7@gmail.com>
2026-01-27 22:53:32 +0100

    Create DTO

---
+++ CleanPoke/Data/DTO/PokemonDTO.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDTO.swift
+//  CleanPoke
+//
+//  Created by Digiprem on 27/01/2026.
+//
+
+import Foundation
---
+++ CleanPoke/Data/DTO/PokemonResultDTO.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonResultDTO.swift
+//  CleanPoke
+//
+//  Created by Digiprem on 27/01/2026.
+//
+
+import Foundation
