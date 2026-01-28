b6a9ca42e9a3b59e40d96f1b174d33dea84da795
adrien nana <kakonana7@gmail.com>
2026-01-28 07:58:49 +0100

    Create mapper ,entites and repository

---
+++ CleanPoke/Data/Mappers/PokemonMapper.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonMapper.swift
+//  CleanPoke
+//
+//  Created by Digiprem on 27/01/2026.
+//
+
+import Foundation
---
+++ CleanPoke/Data/Repositories/PokemonRepository.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonRepository.swift
+//  CleanPoke
+//
+//  Created by Digiprem on 28/01/2026.
+//
+
+import Foundation
